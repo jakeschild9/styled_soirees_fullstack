25a893890e074ea4845df9cc4d6dcf00cf4653b4
Jake Schildknecht <115961597+jakeschild9@users.noreply.github.com>
2025-02-22 14:19:09 -0600

    tls update

--- Backend/app/mail.js
+++ Backend/app/mail.js
@@ -4,6 +4,11 @@ const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
   secure: true,
+  tls: {
+    maxVersion: 'TLSv1.3',
+    minVersion: 'TLSv1.2',
+    ciphers: 'TLS_AES_128_GCM_SHA256',
+},
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
