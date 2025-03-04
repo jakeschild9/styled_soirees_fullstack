8587c3e273b8fa153b51e846f416235dff89b6ea
Jake Schildknecht <115961597+jakeschild9@users.noreply.github.com>
2025-03-03 21:42:05 -0600

    https only

--- Backend/server.js
+++ Backend/server.js
@@ -49,7 +49,3 @@ app.post("/api/send-email", (req, res) => {
 https.createServer(app).listen(process.env.SERVER_PORT, () => {
   console.log(`Server is running on port ${process.env.SERVER_PORT}.`);
 });
-
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server is running on port ${process.env.SERVER_PORT}.`);
-});
