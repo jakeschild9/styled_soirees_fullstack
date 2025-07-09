a91ab102293995f8d0a4f5b997bc9bfb3f150718
Jake Schildknecht <115961597+jakeschild9@users.noreply.github.com>
2025-07-09 16:18:58 -0500

    minor footer chnage

--- Frontend/src/components/Footer.vue
+++ Frontend/src/components/Footer.vue
@@ -4,7 +4,7 @@
     <div class="">
         <footer class="bg-black h-40 px-10 w-screen flex justify-between items-center align-baseline" id="footer">
             <div class="flex justify-center align-middle font-serif md:text-lg text-sm text-slate-200">
-                Made By - Jake Schildknecht
+                Made By Jake Schildknecht
             </div>
             <div class="align-middle font-serif text-sm md:text-lg text-slate-200">
                 <a> Email styledsoirees@outlook.com</a>
